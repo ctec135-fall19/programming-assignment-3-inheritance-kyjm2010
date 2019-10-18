c5f475cb85049fc872d3eaf6667e01cbe1979426
Kyle McDonald <kyjm2010@gmail.com>
2019-10-18 10:45:43 -0700

    Push 1

---
+++ Problem 1 Solution/Problem 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle a = new Vehicle("Ford", 20102335);
+            SportsCar sc = new SportsCar();
+            a.PrintState();
+            sc.PrintState();
+            a.SetMake("Chevrolet");
+            a.SetInv(90909090);
+            a.PrintState();
+            sc.PrintState();
+            sc.GetMake();
+            sc.VehicleMethod();
+        }
+
+        
+    }
+}
---
+++ Problem 1 Solution/Problem 2/Vehicle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Vehicle
+    {
+        //field
+
+        protected string vehicleMake;
+        private int inventoryNumber;
+
+        //constructors
+
+        public Vehicle() { }
+        public Vehicle(string Make, int invNum)
+        {
+            this.vehicleMake = Make;
+            this.inventoryNumber = invNum;
+        }
+
+        //methods
+        public void VehicleMethod()
+        {
+            Console.WriteLine("This is a message that prints from the parent class");
+        }
+
+        public string GetMake() 
+        { 
+            return vehicleMake; 
+        }
+
+        public void SetMake(string Make)
+        {
+            vehicleMake = Make;
+        }
+
+        public int GetInvId()
+        {
+            return inventoryNumber;
+        }
+
+        public void SetInv(int Num)
+        {
+            inventoryNumber = Num;
+        }
+
+        public virtual void PrintState()
+        {
+            Console.WriteLine("Vehicle Object");
+            Console.WriteLine("\tVehicle Make: {0}", vehicleMake);
+            Console.WriteLine("\tThis vehicle is in lot: {0}", inventoryNumber);
+        }
+
+    }
+}
